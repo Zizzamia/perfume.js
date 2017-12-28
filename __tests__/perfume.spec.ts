0aecf6c1ad208d3b3db1b594ee61f5f87a581108
Thomas Potaire <thomas@getplan.co>
2017-12-28 14:48:05 -0800

    build(lint): Remove prettier, adjust tslint, fix linting errors

--- __tests__/perfume.spec.ts
+++ __tests__/perfume.spec.ts
@@ -1,64 +1,62 @@
-import Perfume from '../src/perfume'
+import Perfume from "../src/perfume";
 
 /**
  * Perfume test
  */
-describe('Perfume test', () => {
-  let perfume
+describe("Perfume test", () => {
+  let perfume;
 
   beforeAll(() => {
     window.performance = {
-      now: (() => {
-        return Date.now
-      })()
-    }
-  })
+      now: Date.now,
+    };
+  });
 
   beforeEach(() => {
-    perfume = new Perfume()
-  })
+    perfume = new Perfume();
+  });
 
-  it('works if true is truthy', () => {
-    expect(true).toBeTruthy()
-  })
+  it("works if true is truthy", () => {
+    expect(true).toBeTruthy();
+  });
 
-  it('Perfume is instantiable', () => {
-    expect(perfume).toBeInstanceOf(Perfume)
-  })
+  it("Perfume is instantiable", () => {
+    expect(perfume).toBeInstanceOf(Perfume);
+  });
 
   it("has 'supportsPerfNow' method after initialization", () => {
-    expect(perfume.supportsPerfNow).toBeDefined()
-  })
+    expect(perfume.supportsPerfNow).toBeDefined();
+  });
 
   it("has 'supportsPerfMark' method after initialization", () => {
-    expect(perfume.supportsPerfMark).toBeDefined()
-  })
+    expect(perfume.supportsPerfMark).toBeDefined();
+  });
 
   it("has 'getMeasurementForGivenName' method after initialization", () => {
-    expect(perfume.getMeasurementForGivenName).toBeDefined()
-  })
+    expect(perfume.getMeasurementForGivenName).toBeDefined();
+  });
 
   it("has 'checkMetricName' method after initialization", () => {
-    expect(perfume.checkMetricName).toBeDefined()
-  })
+    expect(perfume.checkMetricName).toBeDefined();
+  });
 
   it("has 'start' method after initialization", () => {
-    expect(perfume.start).toBeDefined()
-  })
+    expect(perfume.start).toBeDefined();
+  });
 
   it("has 'end' method after initialization", () => {
-    expect(perfume.end).toBeDefined()
-  })
+    expect(perfume.end).toBeDefined();
+  });
 
   it("has 'getFirstPaint' method after initialization", () => {
-    expect(perfume.getFirstPaint).toBeDefined()
-  })
+    expect(perfume.getFirstPaint).toBeDefined();
+  });
 
   it("has 'firstPaint' method after initialization", () => {
-    expect(perfume.firstPaint).toBeDefined()
-  })
+    expect(perfume.firstPaint).toBeDefined();
+  });
 
   it("has 'log' method after initialization", () => {
-    expect(perfume.log).toBeDefined()
-  })
-})
+    expect(perfume.log).toBeDefined();
+  });
+});
--- src/perfume.ts
+++ src/perfume.ts
@@ -1,19 +1,19 @@
 export default class Perfume {
   private metrics: {
     [key: string]: {
-      start: number
-      end: number
-      duration: number
-    }
-  }
-  private logPrefix: string
+      start: number;
+      end: number;
+      duration: number;
+    };
+  };
+  private logPrefix: string;
 
   constructor() {
-    this.metrics = {}
-    this.logPrefix = '⚡️ Perfume.js: '
+    this.metrics = {};
+    this.logPrefix = "⚡️ Perfume.js: ";
 
     if (!this.supportsPerfNow) {
-      throw Error(this.logPrefix + ' Cannot be used in this browser.')
+      throw Error(this.logPrefix + " Cannot be used in this browser.");
     }
   }
 
@@ -22,7 +22,7 @@ export default class Perfume {
    * @type {boolean}
    */
   get supportsPerfNow() {
-    return Boolean(window.performance && performance.now)
+    return Boolean(window.performance && performance.now);
   }
 
   /**
@@ -31,7 +31,7 @@ export default class Perfume {
    * @type {boolean}
    */
   get supportsPerfMark() {
-    return Boolean(window.performance && performance.mark)
+    return Boolean(window.performance && performance.mark);
   }
 
   /**
@@ -39,7 +39,7 @@ export default class Perfume {
    * name. Return the first one found.
    */
   public getMeasurementForGivenName(metricName: string) {
-    return performance.getEntriesByName(metricName)[0]
+    return performance.getEntriesByName(metricName)[0];
   }
 
   /**
@@ -49,14 +49,14 @@ export default class Perfume {
    * performance.now() fallback.
    */
   set duration(metricName: string) {
-    let duration = this.metrics[metricName].end - this.metrics[metricName].start
+    let duration = this.metrics[metricName].end - this.metrics[metricName].start;
     if (this.supportsPerfMark) {
-      const entry = this.getMeasurementForGivenName(metricName)
-      if (entry && entry.entryType !== 'measure') {
-        duration = entry.duration
+      const entry = this.getMeasurementForGivenName(metricName);
+      if (entry && entry.entryType !== "measure") {
+        duration = entry.duration;
       }
     }
-    this.metrics[metricName].duration = duration || -1
+    this.metrics[metricName].duration = duration || -1;
   }
 
   /**
@@ -64,26 +64,26 @@ export default class Perfume {
    */
   public checkMetricName(metricName: string) {
     if (metricName) {
-      return
+      return;
     }
-    throw Error(this.logPrefix + 'Please provide a metric name')
+    throw Error(this.logPrefix + "Please provide a metric name");
   }
 
   /**
    *
    */
   public start(metricName: string) {
-    this.checkMetricName(metricName)
+    this.checkMetricName(metricName);
     if (!this.supportsPerfMark) {
-      global.console.warn(this.logPrefix, `Timeline won"t be marked for "${metricName}".`)
+      global.console.warn(this.logPrefix, `Timeline won"t be marked for "${metricName}".`);
     }
     if (this.metrics[metricName]) {
-      global.console.warn(this.logPrefix, 'Recording already started.')
-      return
+      global.console.warn(this.logPrefix, "Recording already started.");
+      return;
     }
-    this.metrics[metricName].start = performance.now()
+    this.metrics[metricName].start = performance.now();
     if (this.supportsPerfMark) {
-      performance.mark(`mark_${metricName}_start`)
+      performance.mark(`mark_${metricName}_start`);
     }
   }
 
@@ -91,23 +91,23 @@ export default class Perfume {
    *
    */
   public end(metricName: string, log = false, destroy = false) {
-    this.checkMetricName(metricName)
+    this.checkMetricName(metricName);
     if (!this.metrics[metricName]) {
-      global.console.warn(this.logPrefix, 'Recording already stopped.')
-      return
+      global.console.warn(this.logPrefix, "Recording already stopped.");
+      return;
     }
-    this.metrics[metricName].end = performance.now()
+    this.metrics[metricName].end = performance.now();
     if (this.supportsPerfMark) {
-      const startMark = `mark_${metricName}_start`
-      const endMark = `mark_${metricName}_end`
-      performance.mark(endMark)
-      performance.measure(metricName, startMark, endMark)
+      const startMark = `mark_${metricName}_start`;
+      const endMark = `mark_${metricName}_end`;
+      performance.mark(endMark);
+      performance.measure(metricName, startMark, endMark);
     }
     if (log) {
-      this.log(metricName, this.metrics[metricName].duration)
+      this.log(metricName, this.metrics[metricName].duration);
     }
     if (destroy) {
-      delete this.metrics[metricName]
+      delete this.metrics[metricName];
     }
   }
 
@@ -116,12 +116,12 @@ export default class Perfume {
    */
   public getFirstPaint() {
     if (performance) {
-      const navTiming = performance.timing
+      const navTiming = performance.timing;
       if (navTiming && navTiming.navigationStart !== 0) {
-        return Date.now() - navTiming.navigationStart
+        return Date.now() - navTiming.navigationStart;
       }
     }
-    return null
+    return null;
   }
 
   /**
@@ -130,19 +130,19 @@ export default class Perfume {
    */
   public firstPaint() {
     setTimeout(() => {
-      const fp = this.getFirstPaint()
+      const fp = this.getFirstPaint();
       if (fp) {
-        this.log('firstPaint', fp)
+        this.log("firstPaint", fp);
       }
-    })
+    });
   }
 
   /**
    *
    */
   public log(metricName: string, duration: number) {
-    const style = 'color: #ff6d00;font-size:12px;'
-    const text = `%c ${this.logPrefix} ${metricName} ${duration} ms`
-    global.console.log(text, style)
+    const style = "color: #ff6d00;font-size:12px;";
+    const text = `%c ${this.logPrefix} ${metricName} ${duration} ms`;
+    global.console.log(text, style);
   }
 }
