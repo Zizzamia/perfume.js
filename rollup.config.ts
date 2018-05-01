f4fa77fe6ada21aa61a5d0006a428a327b94fd94
Leonardo Zizzamia <leonardo.zizzamia@gmail.com>
2018-04-30 22:00:45 -0700

    build: Minify for all format

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,28 +1,30 @@
-import resolve from 'rollup-plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
-import sourceMaps from 'rollup-plugin-sourcemaps';
-import uglify from 'rollup-plugin-uglify';
-import pkg from './package.json';
+import resolve from "rollup-plugin-node-resolve";
+import commonjs from "rollup-plugin-commonjs";
+import sourceMaps from "rollup-plugin-sourcemaps";
+import uglify from "rollup-plugin-uglify";
+import pkg from "./package.json";
 
-const ensureArray = maybeArr => Array.isArray(maybeArr) ? maybeArr : [maybeArr]
+const ensureArray = maybeArr =>
+  Array.isArray(maybeArr) ? maybeArr : [maybeArr];
 
 const createConfig = ({
   output,
   includeExternals = false,
-  min = false,
+  min = false
 } = {}) => ({
-  input: `dist/es/perfume.js`,
-  output: ensureArray(output).map(format => Object.assign(
-    {},
-    format,
-    { sourcemap: true },
-  )),
+  input: "dist/es/perfume.js",
+  output: ensureArray(output).map(format =>
+    Object.assign({}, format, { sourcemap: true })
+  ),
   // Indicate here external modules you don't wanna include in your bundle (i.e.: 'lodash')
   external: includeExternals
     ? []
-    : [...Object.keys(pkg.dependencies || {}), ...Object.keys(pkg.peerDependencies || {})],
+    : [
+        ...Object.keys(pkg.dependencies || {}),
+        ...Object.keys(pkg.peerDependencies || {})
+      ],
   watch: {
-    include: 'dist/es/**',
+    include: "dist/es/**"
   },
   plugins: [
     // Allow bundling cjs modules (unlike webpack, rollup doesn't understand cjs)
@@ -34,35 +36,56 @@ const createConfig = ({
 
     // Resolve source maps to the original source
     sourceMaps(),
-    min && uglify({
-      output: {
-        comments: function(node, comment) {
-          const { text, type } = comment;
-
-          if (type == "comment2") {
-            // multiline comment
-            return /@preserve|@license|@cc_on/i.test(text);
+    min &&
+      uglify({
+        output: {
+          comments: function(node, comment) {
+            const { text, type } = comment;
+            if (type == "comment2") {
+              // multiline comment
+              return /@preserve|@license|@cc_on/i.test(text);
+            }
           }
         }
-      }
-    }),
-  ].filter(Boolean),
-})
+      })
+  ].filter(Boolean)
+});
 
 export default [
   createConfig({
     output: [
-      { file: pkg.module, format: 'es' },
-      { file: pkg.main, format: 'cjs' }
-    ],
+      { file: pkg.module, format: "es" },
+      { file: pkg.main, format: "cjs" }
+    ]
   }),
   createConfig({
-    output: { file: pkg.iife, name: 'perfume', format: 'iife' },
+    output: { file: "dist/perfume.es5.min.js", name: "perfume", format: "es" },
+    min: true
+  }),
+  createConfig({
+    output: { file: "dist/perfume.min.js", name: "perfume", format: "cjs" },
+    min: true
+  }),
+  createConfig({
+    output: { file: pkg.iife, name: "perfume", format: "iife" },
+    includeExternals: true
+  }),
+  createConfig({
+    output: { file: pkg.iife, name: "perfume", format: "iife" },
+    includeExternals: true
+  }),
+  createConfig({
+    output: { file: "dist/perfume.iife.min.js", name: "perfume", format: "iife" },
+    includeExternals: true,
+    min: true
+  }),
+  createConfig({
+    output: { file: pkg.unpkg, name: "perfume.umd", format: "umd" },
     includeExternals: true,
   }),
   createConfig({
-    output: { file: pkg.unpkg, name: 'perfume.umd', format: 'umd' },
+    output: { file: "dist/perfume.umd.min.js", name: "perfume.umd", format: "umd" },
     includeExternals: true,
-    min: true,
-  }),
-]
+    min: true
+  })
+];
