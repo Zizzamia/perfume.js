436cc037d58b11906677d2da3b5895354cdc9fa1
Stefano Magni <nori.ste.magni@gmail.com>
2020-05-20 08:16:10 +0200

    fix: update generateTrackerData after the code review

--- src/generateTrackerData.ts
+++ src/generateTrackerData.ts
@@ -1,34 +1,56 @@
-import { IAnalyticsTrackerOptions } from './types';
+import {
+  IPerfumeData,
+  IAnalyticsTrackerOptions,
+  IPerfumeNavigationTiming,
+  IPerfumeNetworkInformation,
+} from './types';
 
-export const generateTrackerData = (
-  options: IAnalyticsTrackerOptions,
-): { metricName: string; duration?: number; value?: any } => {
+export const generateTrackerData = (options: IAnalyticsTrackerOptions) => {
+  const isIPerfumeDataType = <T extends IPerfumeData>(
+    iPerfumeData: IPerfumeData,
+  ): iPerfumeData is T => {
+    const data = iPerfumeData as T;
+    if (typeof iPerfumeData !== 'object') return false;
+
+    const dataKeys = Object.keys(data) as (keyof T)[];
+    for (const key of dataKeys) if (data[key] !== undefined) return true;
+    return false;
+  };
+
   const { metricName, data } = options;
   switch (metricName) {
     case 'navigationTiming':
-      if (data && data.timeToFirstByte) {
-        return { metricName: 'navigationTiming', duration: data };
+      if (
+        data &&
+        isIPerfumeDataType<IPerfumeNavigationTiming>(data) &&
+        data.timeToFirstByte
+      ) {
+        return { metricName: 'navigationTiming', data };
       }
       break;
     case 'networkInformation':
-      if (data && data.effectiveType) {
-        return { metricName: 'networkInformation', duration: data };
+      if (
+        data &&
+        isIPerfumeDataType<IPerfumeNetworkInformation>(data) &&
+        data.effectiveType
+      ) {
+        return { metricName: 'networkInformation', data };
       }
       break;
     case 'storageEstimate':
-      return { metricName: 'storageEstimate', duration: data };
+      return { metricName: 'storageEstimate', data };
       break;
     case 'fp':
-      return { metricName: 'firstPaint', duration: data.duration };
+      return { metricName: 'firstPaint', duration: data };
       break;
     case 'fcp':
-      return { metricName: 'firstContentfulPaint', duration: data.duration };
+      return { metricName: 'firstContentfulPaint', duration: data };
       break;
     case 'fid':
-      return { metricName: 'firstInputDelay', duration: data.duration };
+      return { metricName: 'firstInputDelay', duration: data };
       break;
     case 'lcp':
-      return { metricName: 'largestContentfulPaint', duration: data.duration };
+      return { metricName: 'largestContentfulPaint', duration: data };
       break;
     case 'lcpFinal':
       return { metricName: 'largestContentfulPaintFinal', duration: data };
@@ -40,21 +62,19 @@ export const generateTrackerData = (
       return { metricName: 'cumulativeLayoutShiftFinal', value: data };
       break;
     case 'tbt':
-      return { metricName: 'totalBlockingTime', duration: data.duration };
+      return { metricName: 'totalBlockingTime', duration: data };
       break;
     case 'tbt5S':
-      return { metricName: 'totalBlockingTime5S', duration: data.duration };
+      return { metricName: 'totalBlockingTime5S', duration: data };
       break;
     case 'tbt10S':
-      return { metricName: 'totalBlockingTime10S', duration: data.duration };
+      return { metricName: 'totalBlockingTime10S', duration: data };
       break;
     case 'tbtFinal':
-      return { metricName: 'totalBlockingTimeFinal', duration: data.duration };
+      return { metricName: 'totalBlockingTimeFinal', duration: data };
       break;
     default:
-      return { metricName, duration: data.duration };
+      return { metricName: metricName, duration: data };
       break;
   }
-
-  return { metricName, duration: data.duration };
 };
