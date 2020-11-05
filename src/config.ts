b6bb979e1a8f75bd230ef2b492b69f9ec96dba98
Klaus L. Hougesen <klaus.hougesen@gmail.com>
2020-11-05 10:44:16 +0100

    Update config.ts
    added doNotTrack respect

--- src/config.ts
+++ src/config.ts
@@ -3,6 +3,7 @@ import { IPerfumeConfig } from './types';
 export const config: IPerfumeConfig = {
   // Metrics
   isResourceTiming: false,
+  respectDoNoTrack: true,
   isElementTiming: false,
   // Logging
   maxTime: 15000,
