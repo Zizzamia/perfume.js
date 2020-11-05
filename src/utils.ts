940399478ef6d9a028e49f719c39347a7f4ea71f
Klaus L. Hougesen <klaus.hougesen@gmail.com>
2020-11-05 10:57:49 +0100

    Update utils.ts

--- src/utils.ts
+++ src/utils.ts
@@ -22,7 +22,7 @@ export const pushTask = (cb: any): void => {
   }
 };
 
-export const getTrack = (): boolean => {
+export const getDoNotTrack = (): boolean => {
   if (window.doNotTrack || navigator.doNotTrack || navigator.msDoNotTrack || 'msTrackingProtectionEnabled' in window.external) {
     if (window.doNotTrack == "1" || navigator.doNotTrack == "yes" || navigator.doNotTrack == "1" || navigator.msDoNotTrack == "1" || window.external.msTrackingProtectionEnabled()) {
         return true
