ae3b260bfc65f1c1e2e37a0b3adce8fc153dd2cc
Klaus L. Hougesen <klaus.hougesen@gmail.com>
2020-11-05 10:57:33 +0100

    Update utils.ts
    added more generalized getTrack function

--- src/utils.ts
+++ src/utils.ts
@@ -21,3 +21,15 @@ export const pushTask = (cb: any): void => {
     cb();
   }
 };
+
+export const getTrack = (): boolean => {
+  if (window.doNotTrack || navigator.doNotTrack || navigator.msDoNotTrack || 'msTrackingProtectionEnabled' in window.external) {
+    if (window.doNotTrack == "1" || navigator.doNotTrack == "yes" || navigator.doNotTrack == "1" || navigator.msDoNotTrack == "1" || window.external.msTrackingProtectionEnabled()) {
+        return true
+    } else {
+        return false
+    }
+  } else {
+      return false
+  }
+}
