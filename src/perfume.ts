d2b83aa0d2d9c0e90451550ca38da5292c90e6f5
Cooper Willetts <willettscooper9@gmail.com>
2023-12-04 12:17:55 -0500

    added markntbt to export

--- src/perfume.ts
+++ src/perfume.ts
@@ -3,7 +3,17 @@ import { initPerfume } from './initPerfume';
 import { clear, end, markStep, start } from './steps/markStep';
 import { markStepOnce } from './steps/markStepOnce';
 import { trackUJNavigation } from './steps/navigationSteps';
+import { markNTBT } from './markNTBT';
 
 export * from './types';
 
-export { clear, end, initPerfume, markStep, markStepOnce, start, trackUJNavigation };
+export {
+  clear,
+  end,
+  initPerfume,
+  markNTBT,
+  markStep,
+  markStepOnce,
+  start,
+  trackUJNavigation,
+};
