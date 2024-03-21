f02ac701115d0896434175da16cb841b5ee763e4
Saivann Carignan <saivann.carignan@coinbase.com>
2024-03-21 19:10:32 -0400

    Fix import crash under node.js

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,6 @@
 // Have private variable outside the class,
 // helps drastically reduce the library size
-export const W = window;
+export const W = globalThis;
 export const C = W.console;
 export const WN = W.navigator;
 export const WP = W.performance;
